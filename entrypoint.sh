cea1d635514292dadcb046bea85e5b4078c4ce27
Michal Charemza <michal@charemza.name>
2019-01-15 09:26:09 +0000

    (fix) Do not depend on routes being present to be more CI-pipeline-friendly

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,11 +12,6 @@ if [ -z "${VCAP_SERVICES}" ]; then
   exit 1
 fi
 
-if ! echo ${VCAP_APPLICATION} | jq ".application_uris[0] != null" -e; then
-  echo "The application must have a route"
-  exit 1
-fi
-
 if ! echo ${VCAP_SERVICES} | jq ".elasticsearch[0] != null" -e; then
   echo "You must bind an elasticsearch service to this application"
   exit 1
@@ -27,7 +22,6 @@ python3 -m sso-proxy &
 /usr/share/kibana/bin/kibana --cpu.cgroup.path.override=/ --cpuacct.cgroup.path.override=/ \
   --server.host=127.0.0.1 \
   --server.port=5601 \
-  --server.name=$(echo ${VCAP_APPLICATION} | jq ".application_uris[0]" -r) \
   --elasticsearch.url=https://$(echo ${VCAP_SERVICES} | jq ".elasticsearch[0].credentials.hostname" -r):$(echo ${VCAP_SERVICES} | jq ".elasticsearch[0].credentials.port" -r) \
   --elasticsearch.username=$(echo ${VCAP_SERVICES} | jq ".elasticsearch[0].credentials.username" -r) \
   --elasticsearch.password=$(echo ${VCAP_SERVICES} | jq ".elasticsearch[0].credentials.password" -r) &
